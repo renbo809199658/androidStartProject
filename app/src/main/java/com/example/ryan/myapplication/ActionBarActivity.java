27adc0d42d50b997925505d7ff45aad24ba27925
Ryan Ren <809199658@qq.com>
2018-05-02 10:20:20 +0800

    actionBar and activity lifecircle.

--- app/src/main/java/com/example/ryan/myapplication/ActionBarActivity.java
+++ app/src/main/java/com/example/ryan/myapplication/ActionBarActivity.java
@@ -1,8 +1,15 @@
 package com.example.ryan.myapplication;
 
 import android.app.Activity;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ActionBarActivity extends Activity {
 
@@ -10,5 +17,42 @@ public class ActionBarActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_bar);
+
+        Toast.makeText(this, "yahaha", Toast.LENGTH_LONG).show();
+//        getActionBar().hide();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.main);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch  (item.getItemId()) {
+            case 1:
+            Intent upIntent =  NavUtils.getParentActivityIntent(this);
+           if (NavUtils.shouldUpRecreateTask(this,upIntent)){
+               TaskStackBuilder.create(this).addNextIntentWithParentStack(upIntent).startActivities();
+           } else {
+               upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+           }
+        }
+
+        item.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                return false;
+            }
+        });
+        return super.onOptionsItemSelected(item);
     }
 }
